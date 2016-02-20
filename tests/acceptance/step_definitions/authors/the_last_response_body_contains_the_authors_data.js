47129ed316721a56bd3b6f93dbfa88598142e656
guumaster <gustavoandresmarin@gmail.com>
2016-02-20 17:36:33 +0100

    changed deep equal to loop due to sort issues on CircleCI

--- tests/acceptance/step_definitions/authors/the_last_response_body_contains_the_authors_data.js
+++ tests/acceptance/step_definitions/authors/the_last_response_body_contains_the_authors_data.js
@@ -5,7 +5,9 @@ const expect = require('chai').expect;
 module.exports = function() {
   this.Then(/^the last response body contains the authors data$/, done => {
     const world = this.world;
-    expect(world.lastResponseBody.authors).to.deep.equal(world.authorsToCreate);
+    const authors = world.lastResponseBody.authors;
+    
+    authors.forEach(author => expect(world.authorsToCreate).to.include(author));
     done();
   });
 };
